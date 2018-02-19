4ee90b3dd7b6fda2af12a4c171ab8e7c60cc7db0
Zhen-hui <trinh.z@husky.neu.edu>
2018-02-19 11:07:48 -0500

    typo corrected

--- RNA_Seq/trimAll.sh
+++ RNA_Seq/trimAll.sh
@@ -22,7 +22,7 @@ do
 
 #	echo $sampleName	
 
-	nice -n 19 java -jav /usr/local/programs/Trimmomatic-0.36/trimmomatic-0.36.jar PE \
+	nice -n 19 java -jar /usr/local/programs/Trimmomatic-0.36/trimmomatic-0.36.jar PE \
 	-threads 1 -phred33 \
 	$fastqPath$sampleName$leftSuffix \
 	$fastqPath$sampleName$rightSuffix \
@@ -33,6 +33,6 @@ do
 	HEADCROP:0 \
 	ILLUMINACLIP:/usr/local/programs/Trimmomatic-0.36/adaptors/TruSeq3-PE.fa:2:30:10 \
 	LEADING:20 TRAILING:20 SLIDINGWINDOW:4:30 MINLEN:36 \
-	1>$sampleName.trim.log 2>$sampleName.trim.err
+	1>$sampleName.trim.log 2>$sampleName.trim.err &
  
 done

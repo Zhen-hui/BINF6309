a32dd39b49a62fbf9964a6b83e7196fe4abb323f
Zhen-hui <trinh.z@husky.neu.edu>
2018-02-22 17:25:35 -0500

    mergeAnnotations.pl modified

--- RNA_Seq/mergeAnnotations.pl
+++ RNA_Seq/mergeAnnotations.pl
@@ -1,5 +1,7 @@
 #!/bin/perl
 use Bio::SearchIO;
+use Bio::Search::Result::GenericResult;
+use Data::Dumper;
 
 my $blastXml = Bio::SearchIO->new(
 	-file   => 'Trinity-GG.blastp.xml',
@@ -7,5 +9,22 @@ my $blastXml = Bio::SearchIO->new(
 );
 
 while (my $result = $blastXml -> next_result()) {
-	print $result, "\n";
+	my $queryDesc = $result->query_description;
+	if ($queryDesc =~ /::(.*?)::/) {
+		my $queryDescShort = $1;
+		my $hit   = $result->next_hit;
+			if ($hit) {
+			print $queryDescShort, "\t";
+			print $hit->accession, "\t";			
+			my $subjectDescription = $hit->description;
+			if ($subjectDescription =~ /Full=(.*?);/){
+				$subjectDescription = $1;
+			}
+			if ($subjectDescription =~ /Full=(.*?)\[/){
+				$subjectDescription = $1;
+			}
+			print $subjectDescription, "\t";
+			print $hit->significance, "\n";
+		}
+	}
 }

a1ebb27b8a44f531f1690228af2819031b569ac2
Zhen-hui <trinh.z@husky.neu.edu>
2018-02-24 14:29:46 -0500

    create mergeAnnotationsGo.pl

--- RNA_Seq/mergeAnnotationsGO.pl
+++ RNA_Seq/mergeAnnotationsGO.pl
@@ -11,7 +11,18 @@ while (<SP_TO_GO>) {
 	my ($swissProt, $go) = split("\t",$_);
 	$spToGo{$swissProt}{$go}++;
 }
+open (SP, "<", "aipSwissProt.tsv") or die $!;
 
+while (<SP>) {
+	chomp;
+	my ($trinity, $swissProt, $description, $eValue) = split("\t",$_);
+	if (defined $spToGo{$swissProt}) {
+		foreach my $go (sort keys %{$spToGo{$swissProt}}){
+			print join("\t",$trinity,$description,$swissProt,$go),"\n";
+		}
+	}
+}
+=cut
 foreach my $swissProt(sort keys %spToGo){
 	foreach my $go ( sort keys %{$spToGo{$swissProt}} ) {
 		print join("\t",$swissProt,$go),"\n";

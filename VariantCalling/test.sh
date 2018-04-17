231191a7316ed0c9839188750bc3f35d8cd757b0
Zhen-hui <trinh.z@husky.neu.edu>
2018-04-17 13:22:07 -0400

    test script for recalibrating bases

--- VariantCalling/test.sh
+++ VariantCalling/test.sh
@@ -2,11 +2,15 @@
 
 # Step 1: Analyze patterns of covariation in the sequence dataset
 
+bam="$(ls -q noDup/*.bam)"
+#echo $bam
+
 baseCommand='nice -n19 java -jar /usr/local/programs/GenomeAnalysisTK-3.8-0/GenomeAnalysisTK.jar'
 
 $baseCommand -T BaseRecalibrator \
 	     -R vShiloni.fasta \
-  	     -I noDup/SRR5231228.bam \
+ 	     -I $bam \
 	     -knownSites filteredSnps.vcf \
  	     -o recal.table \
-1>recalibrate.log 2>recalibrate.err &
+1>test.log 2>test.err &
+ 

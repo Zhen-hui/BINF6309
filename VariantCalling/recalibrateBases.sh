a53a825a50f6c0678b9188c9a250eaedb4cb3d9b
Zhen-hui <trinh.z@husky.neu.edu>
2018-04-18 19:06:28 -0400

    recalibrateBases

--- VariantCalling/recalibrateBases.sh
+++ VariantCalling/recalibrateBases.sh
@@ -1,30 +1,42 @@
 #!/bin/bash
 
+# Store all bam files in bamFiles 
+bamFiles="$(ls -q noDup/*.bam)"
+#echo $bamFiles
+
+inPath='noDup/'
+# Put an -I in front of each bam file
+replacement='-I noDup/'
+bam="${bamFiles//$inPath/$replacement}"
+#echo $bam
+
 # Step 1: Analyze patterns of covariation in the sequence dataset
-# Create GATK report file containing several tables. These tables contain the covariation data that will used in a later step to recalibrate the base qualities of the sequence data. 
-bamDir='noDup/'
-bamSuffix='.bam'
-
-# Create a directory for the table outputs 
-tableDir='tables/'
-mkdir -p $tableDir
-
-# Define a function to recalibrate the bases 
-recalibrateBases(){
-
-# Loop through every bam file in the noDup directory 
-for bamFile in $bamDir*$bamSuffix
-do 
-
-outFile="${bamFile/$bamDir/$tableDir}"
-outFile="${outFile/bamFile/$tableDir}"
+# This will create a table containing the covariation data that will be used in a later step to recalibrate the base qualities of the sequence data.
 baseCommand='nice -n19 java -jar /usr/local/programs/GenomeAnalysisTK-3.8-0/GenomeAnalysisTK.jar'
 
 $baseCommand -T BaseRecalibrator \
 	     -R vShiloni.fasta \
-  	     -I $bamFile \
+ 	     $bam \
 	     -knownSites filteredSnps.vcf \
- 	     -o $outFile.table  
-done 
-}
-recalibrateBases 1>recalibrate.log 2>recalibrate.err &
+	     -o recal.table \
+1>recalibrateBases.log 2>recalibrateBases.err &&
+
+# Step 2: do a second pass to analyze covariation remaining after recalibration 
+# This creates another GATKReport file to be used for plotting. -BQSR parameter tells GATK engine to perform on-the-fly recalibration based on the first recalibration data table
+$baseCommand -T BaseRecalibrator \
+	     -R vShiloni.fasta \
+	     $bam \
+	     -knownSites filteredSnps.vcf \
+	     -BQSR recal.table \
+	     -o post_recal.table \
+1>post.log 2>post.err &&
+
+# Step 3: Generate before/after plots
+$baseCommand -T AnalyzeCovariates \
+	     -R vShiloni.fasta \
+	     -before recal.table \
+	     -after post_recal.table \
+	     -plots test_plots.pdf \
+1>plot.log 2>plot.err &
+ 
+ 

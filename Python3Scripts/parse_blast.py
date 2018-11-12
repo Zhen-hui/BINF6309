e8e1ac7bd347729471eb5bc012ea8d7ac6e140a0
Zhen-hui <trinh.z@husky.neu.edu>
2018-11-11 19:49:20 -0800

    Parse BLAST file

---
+++ Python3Scripts/parse_blast.py
@@ -0,0 +1,24 @@
+'''
+Created on Oct 19, 2018
+
+@author: zhentrinh
+'''
+
+# Read in file 
+blastfile = open("../scratch/RNASeq/blastp.outfmt6")
+all_lines = blastfile.readlines()
+
+# Open file for writing
+output = open("parsed_blast.txt", "w")
+
+# Grab the transcript ID, isoform, SwissProt ID, and percent of identical matches
+for line in all_lines:
+    element = line.replace('|', '\t').split()
+    transcriptID = element[0]
+    isoform = element[1]
+    SwissProtID = element[5].split(".")[0] 
+    pident = element[7] 
+    output.write(transcriptID  + '\t' + isoform +  '\t' + SwissProtID +  '\t' + pident + '\n')
+
+blastfile.close()
+output.close()

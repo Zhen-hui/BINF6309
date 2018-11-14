c28e6b11063161be3c30780c2a2964a1cf22ed23
Zhen-hui <trinh.z@husky.neu.edu>
2018-11-13 19:54:43 -0800

    Convert codes to OOP

--- Python3Scripts/parse_go_terms.py
+++ Python3Scripts/parse_go_terms.py
@@ -4,43 +4,51 @@ Created on Oct 30, 2018
 @author: cathytrinh
 '''
 import re
+import os
 
-# Define a function for splitting the file into records
-def records(file_path):
+# define a class that will contain the necessary attribute values of a single GO term record.
+class GO_attributes():
+    
+    def __init__(self, term):
+        ID_pattern        = re.compile(r"^id:\s+(GO:[0-9]+)", re.M)
+        name_pattern      = re.compile(r"^name:\s+(.*)\s+", re.M)
+        namespace_pattern = re.compile(r"^namespace:\s+(.*?)\s+", re.M)
+        is_a_pattern      = re.compile(r"^is_a:\s+(GO:[0-9]+.*?\n)", re.M)
+         
+        self.ID        = re.findall(ID_pattern, term)  
+        self.name      = re.findall(name_pattern, term)
+        self.namespace = re.findall(namespace_pattern, term)
+        self.is_as      = re.findall(is_a_pattern, term)
+
+    def writeResult(self, outfile = "Outputs/parsed_go_terms.txt"):    
+        with open (outfile, "a") as p:
+            p.write(self.ID[0] + "\t")
+            p.write(self.namespace[0] + "\n")
+            p.write("\t" + self.name[0] + "\n")
+            for is_a in self.is_as:
+                p.write("\t" + is_a)
+            p.write("\n")
+                    
+# define a function for splitting the file into records.
+def splitRecords(file_path):
     with open(file_path) as f:
-        data = f.read()        
-        data_1 = re.split("\[Term\]|\[Typedef\]", data)        
-        return data_1
+        allData = f.read()        
+        go_terms = re.split("\[Term\]", allData)       
 
-# test_record = records("/Users/cathytrinh/Desktop/BINF6200/Module01/scratch/go-basic.obo")[1]
-# print test_record
+        # add each valid GO term object to a dictionary.
+        GO_dict = {}
+        for go_term in go_terms:   
+            info = GO_attributes(go_term)
+            if info.ID:
+                GO_dict.update({info.ID[0]: info})
+                
+        # iterate through the dictionary and invoke the object's printing method
+        for k in sorted (GO_dict.keys()):
+            GO_dict[k].writeResult()        
 
-# Define a function to parse and capture the following fields of a single GO term. 
-def parseGO(term):
-    ID_pattern = re.compile(r"^id:\s+(GO:[0-9]+)", re.M)
-    name_pattern = re.compile(r"^name:\s+(.*)\s+", re.M)
-    namespace_pattern = re.compile(r"^namespace:\s+(.*?)\s+", re.M)
-    is_a_pattern = re.compile(r"^is_a:\s+(.*?\s+.*)", re.M)
-    
-    ID = re.findall(ID_pattern, term)
-    name = re.findall(name_pattern, term)
-    namespace = re.findall(namespace_pattern, term)
-    is_a = re.findall(is_a_pattern, term)
-    key_inDict = "".join(ID)
-    value_inDict = "\n\t".join(namespace + name + is_a)
 
-    return key_inDict, value_inDict
-
-# print(parseGO(test_record)[0])
-# print(parseGO(test_record)[1])
-# 
-# Use the parseGO function to store the returned values in a dictionary
-GO_dict = {}
-allrecords = records("../scratch/go-basic.obo")[1:-5]
-for record in allrecords:
-    GO_dict.update({parseGO(record)[0]:parseGO(record)[1]})
-    
-# Iterate through the dictionary and print each parsed record to file
-with open ("Outputs/parsed_go_terms.txt", "w") as p:
-    for k in sorted (GO_dict.keys()):
-        p.write("%s\t%s \n\n" % (k, GO_dict[k]))
+if os.path.exists("Outputs/parsed_go_terms.txt"):
+    os.remove("Outputs/parsed_go_terms.txt")
+    splitRecords("../scratch/go-basic.obo")
+else:          
+    splitRecords("../scratch/go-basic.obo")
